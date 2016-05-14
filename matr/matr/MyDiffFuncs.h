969494a4c503194972839c18bc42504f672f380b
Karkusha93 <netali-2604@mail.ru>
2016-05-14 12:12:09 +0300

    Методы решения диф уравнений

---
+++ matr/matr/MyDiffFuncs.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <omp.h>
+
+class MyDiffFuncs
+{
+public:
+	typedef double (*FType)(double x, double* y);
+    FType F;
+
+	double* Eiler(void** Function, double a, double b, int n, double* y, int count);
+	double* RungeKutta2(void** Function, double a, double b, int n, double* y, int count);
+	double* RungeKutta4(void** Function, double a, double b, int n, double* y, int count);
+
+	double* Eiler_Paral(void** Function, double a, double b, int n, double* y, int count);
+	double* RungeKutta2_Paral(void** Function, double a, double b, int n, double* y, int count);
+	double* RungeKutta4_Paral(void** Function, double a, double b, int n, double* y, int count);
+
